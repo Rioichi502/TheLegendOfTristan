636a68ff1c6194d0f4a681dc424047eaef32f459
Rioichi502 <Rioichi503@gmail.com>
2020-11-22 19:50:43 +0100

    UI

---
+++ Legend Of Tristan/Assets/Scripts/AjustarVolumen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AjustarVolumen : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    public GameObject MenuUI;
+
+    public void Volumen(float volumen) {
+        audioMixer.SetFloat("volumen", volumen);
+    }
+
+    public void setFullScreen(bool FullScreen) {
+        Screen.fullScreen = FullScreen;
+    }
+
+    public void ResumeGame() {
+        MenuUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+}
---
+++ Legend Of Tristan/Assets/Scripts/MenuOpciones.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOpciones : MonoBehaviour
+{
+
+    public GameObject MenuUI;
+
+   public void Opciones()
+    {
+        MenuUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
